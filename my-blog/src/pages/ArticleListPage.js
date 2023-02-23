7f02a38cfdda367e7c4f5dd467de66ffd3dcbab4
Astitwa Raj <astitva007raj@gmail.com>
2023-02-23 20:38:17 +0530

    basic structure

---
+++ my-blog/src/pages/ArticleListPage.js
@@ -0,0 +1,6 @@
+const ArticleListPage = () => {
+  return (
+    <h1>This is the ArticleListPage </h1>
+  );
+}
+export default ArticleListPage;
---
+++ my-blog/src/pages/HomePage.js
@@ -0,0 +1,6 @@
+const HomePage = () => {
+  return (
+    <h1>This is the HomePage </h1>
+  );
+}
+export default HomePage;
